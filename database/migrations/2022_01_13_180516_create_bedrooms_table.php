8eee658310c293a5f59b3b2ff6cee038c60b6266
Sargis Nikolyan <sargisnikolyan@gmail.com>
2022-01-14 06:08:50 +0400

    Added House, Bedroom, Bathroom, Garage, Storey models and there migrations & seeders

---
+++ database/migrations/2022_01_13_180516_create_bedrooms_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBedroomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bedrooms', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bedrooms');
+    }
+}
